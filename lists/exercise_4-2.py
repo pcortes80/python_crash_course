4a2cee268c8559658eeb40d9d6702e885305b14c
Patricio Cortes <patricio.cortes@noirlab.edu>
2023-05-02 21:28:37 -0400

    Add more list exercises

---
+++ lists/exercise_4-2.py
@@ -0,0 +1,4 @@
+animals = ['tiger', 'cat', 'lion', 'puma','lynx']
+print(animals)
+for animal in animals:
+    print(f"A {animal} can be dangerous. Be careful!")
---
+++ lists/first_numbers.py
@@ -0,0 +1,6 @@
+print("for value in range(1,6)")
+for value in range(1, 6):
+    print(value)
+print("list(range(1,6))")
+numbers = list(range(1, 6))
+print(numbers)
