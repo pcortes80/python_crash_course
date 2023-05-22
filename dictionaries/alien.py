7ba371da60ac751df42cb758ac8579b50cd7d07e
Patricio Cortes <patricio.cortes@noirlab.edu>
2023-05-21 22:43:53 -0400

    Add if and dictionaries practices

---
+++ dictionaries/alien.py
@@ -0,0 +1,11 @@
+alien_0 = {} # Define the dictionary
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is {alien_0['color']}.")
+
+del alien_0['points']
+print(alien_0)
