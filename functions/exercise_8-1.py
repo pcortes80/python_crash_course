d8b5f4600e63d8486d0e7425ce36f0c1130cdbbd
Patricio Cortes <patricio.cortes@noirlab.edu>
2023-07-23 17:11:44 -0400

    Add functions part1.

---
+++ functions/exercise_8-1.py
@@ -0,0 +1,8 @@
+def display_message(message):
+    """This will display a message
+    with what I have learned today"""
+
+    print(f"What I´ve learned today was: {message}.")
+
+message = 'a parameter is a value that a function needs to do its job, and an argument is a value that is passed to the function and is assigned to the funcion´s parameter.'
+display_message(message)
---
+++ functions/greeter.py
@@ -0,0 +1,6 @@
+# Functions
+def greeter_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!.")
+
+greeter_user('patricio')
